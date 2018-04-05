649afbdc37e01de0d5f0952acdfea42ef9fc9647
Patryk <drapikpatryk@gmail.com>
2018-04-05 20:07:38 +0200

    first commit

--- src/BitBagSyliusMolliePlugin.php
+++ src/BitBagSyliusMolliePlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace BitBag\SyliusMolliePlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class BitBagSyliusMolliePlugin extends Bundle
 {
     use SyliusPluginTrait;
 }

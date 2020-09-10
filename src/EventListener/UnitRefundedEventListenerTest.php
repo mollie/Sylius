74368fa0f412a1b6e127a170e9e042fde2dd391b
Patryk Drapik <drapikpatryk@gmail.com>
2020-09-10 15:00:52 +0200

    Revert "Add event listener to listen partial refund from sylius to mollie"
    This reverts commit ff6b8df40dce29a56458058fa48d9ff6ddb1ae48.

---
+++ src/EventListener/UnitRefundedEventListenerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BitBag\SyliusMolliePlugin\EventListener;
+
+use Sylius\RefundPlugin\Event\UnitsRefunded;
+
+class UnitRefundedEventListenerTest
+{
+//    public function __invoke(UnitsRefunded $unitRefunded): void
+//    {
+//        dd($unitRefunded);
+//    }
+}

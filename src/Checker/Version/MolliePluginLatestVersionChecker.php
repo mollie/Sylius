390a7559550e83c69440e44b20eb466004971336
Daniel Formela <daniel8702@gmail.com>
2020-12-10 12:22:49 +0100

    Change titles and add templates

--- src/Checker/Version/MolliePluginLatestVersionChecker.php
+++ src/Checker/Version/MolliePluginLatestVersionChecker.php
@@ -3,9 +3,8 @@
 /*
  * This file has been created by developers from BitBag.
  * Feel free to contact us once you face any issues or want to start
- * another great project.
- * You can find more information about us on https://bitbag.shop and write us
- * an email on mikolaj.krol@bitbag.pl.
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
  */
 
 declare(strict_types=1);
@@ -40,7 +39,7 @@ final class MolliePluginLatestVersionChecker implements MolliePluginLatestVersio
         } catch (GuzzleException $exception) {
             return null;
         }
-        
+
         $hubResponse = json_decode($hubResponse->getBody()->getContents(), true);
 
         return $this->getMolliePluginLatestVersion($hubResponse);

54b698324a9ce8be31bdd8c77ec23baffa93ab9f
Marcin Gromek <marcin.gromek@bitbag.pl>
2021-03-26 01:40:34 +0100

    refactor:MOL-98: Sortable payment methods with vanilla JS

--- src/Resources/public/js/Admin/sortable.js
+++ src/Resources/public/js/Admin/sortable.js
@@ -1,8 +1,43 @@
 $(function () {
-  const sortableContainer = $('.js-sortable');
-  if (!sortableContainer.length) {
+  const container = document.querySelector('.js-sortable');
+  if (!container && !container.length) {
     return;
   }
-  sortableContainer.sortable();
-  sortableContainer.disableSelection();
+  const draggables = document.querySelectorAll('.js-draggable');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+      draggable.classList.add('dragging');
+    })
+
+    draggable.addEventListener('dragend', () => {
+      draggable.classList.remove('dragging')
+    });
+  })
+
+  container.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    const afterElement = getDragAfterElement(container, event.clientY);
+    const draggable = document.querySelector('.dragging');
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  })
+
+  function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.js-draggable:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest;
+      }
+
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+  }
 });

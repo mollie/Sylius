a8f9403b7854a0bac30c16fdff68dd11cf7a099e
Marcin Gromek <marcin.gromek@bitbag.pl>
2021-04-16 14:06:29 +0200

    bugfix:MOL-101: Fix wrapping long string - continue

--- src/Resources/public/js/Admin/onboardingWizard.js
+++ src/Resources/public/js/Admin/onboardingWizard.js
@@ -128,7 +128,7 @@ $(function () {
       btnCloseClass: 'btn-close',
     },
     {
-      text: 'Choose Payments API (Payments API can not be used for methods such as Klarna - we\'ll set-up that up' +
+      text: 'Choose Payments API (Payments API can not be used for methods such as Klarna - we\'ll set-up that up\n' +
         ' later) Learn about the difference  between Orders APIor the Payments API',
       stepClass: 'step-10 right-bottom',
       classActive: 'payment-settings',

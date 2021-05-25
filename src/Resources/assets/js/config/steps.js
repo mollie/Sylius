901e01f656eecdeab5a92354df873381c5728010
admin <admin@admins-MacBook-Pro.local>
2021-05-25 16:10:18 +0200

    MOL-115 addition of step logic + styling + visual changes

--- src/Resources/assets/js/config/steps.js
+++ src/Resources/assets/js/config/steps.js
@@ -1,198 +1,262 @@
+import {paymentTypeIndicator} from '../helpers/filterMethod';
+
 export const stepPaymentType = {
-  id: 'step-payment-type',
-  text: 'stepPaymentType.text',
-  classes: 'shepherd-element--align-right',
-  highlightClass: 'payment-settings',
-  attachTo: {
-    element: '.js-onboardingWizard-paymentType',
-    on: 'top-start'
-  },
-  btnNextClass: 'shepherd-button--arrow-down'
+	id: 'step-payment-type',
+	text: 'stepPaymentType.text',
+	classes: 'shepherd-element--align-right',
+	highlightClass: 'payment-settings',
+	attachTo: {
+		element: '.js-onboardingWizard-paymentType',
+		on: 'top-start',
+	},
+	btnNextClass: 'shepherd-button--arrow-down',
 };
 
 export const stepPaymentDescription = {
-  id: 'step-payment-description',
-  text: 'stepPaymentDescription.text',
-  classes: 'shepherd-element--align-right',
-  attachTo: {
-    element: '.js-onboardingWizard-paymentDescription',
-    on: 'top-start'
-  },
-  highlightClass: 'payment-settings',
-  btnNextClass: 'shepherd-button--arrow-down'
+	id: 'step-payment-description',
+	text: 'stepPaymentDescription.text',
+	classes: 'shepherd-element--align-right',
+	attachTo: {
+		element: '.js-onboardingWizard-paymentDescription',
+		on: 'top-start',
+	},
+	highlightClass: 'payment-settings',
+	btnNextClass: 'shepherd-button--arrow-down',
 };
 
 export const stepOrderApi = {
-  id: 'step-order-api',
-  highlightClass: 'payment-settings',
-  classes: 'shepherd-element--align-right',
-  text: 'stepOrderApi.text',
-  attachTo: {
-    element: '.js-onboardingWizard-paymentType',
-    on: 'top-start',
-  },
-  btnNextClass: 'shepherd-button--arrow-down'
+	id: 'step-order-api',
+	highlightClass: 'payment-settings',
+	classes: 'shepherd-element--align-right',
+	text: 'stepOrderApi.text',
+	attachTo: {
+		element: '.js-onboardingWizard-paymentType',
+		on: 'top-start',
+	},
+	btnNextClass: 'shepherd-button--arrow-down',
 };
 
 export const stepQuitConfirmation = [
-  {
-    id: 'step-quit-confirmation',
-    title: 'stepQuitConfirmation.title',
-    text: 'stepQuitConfirmation.text',
-    highlightClass: 'intro',
-    customButtons: [
-      {
-        text: 'stepButtons.quitConfirm',
-        action: (OnboardingWizard) => {
-          OnboardingWizard.tour.removeStep('step-quit-confirmation');
-          OnboardingWizard.tour.complete();
-        },
-        secondary: true,
-      },
-      {
-        text: 'stepButtons.quitCancel',
-        action: (OnboardingWizard, stepIndex) => {
-          OnboardingWizard.tour.show(stepIndex, true);
-          OnboardingWizard.tour.removeStep('step-quit-confirmation');
-        },
-      },
-    ],
-  }
+	{
+		id: 'step-quit-confirmation',
+		title: 'stepQuitConfirmation.title',
+		text: 'stepQuitConfirmation.text',
+		highlightClass: 'intro',
+		customButtons: [
+			{
+				text: 'stepButtons.quitConfirm',
+				action: (OnboardingWizard) => {
+					OnboardingWizard.tour.removeStep('step-quit-confirmation');
+					OnboardingWizard.tour.complete();
+				},
+				secondary: true,
+			},
+			{
+				text: 'stepButtons.quitCancel',
+				action: (OnboardingWizard, stepIndex) => {
+					OnboardingWizard.tour.show(stepIndex, true);
+					OnboardingWizard.tour.removeStep('step-quit-confirmation');
+				},
+			},
+		],
+	},
 ];
 
 export const steps = [
-  {
-    id: 'step-start',
-    title: 'stepStart.title',
-    text: 'stepStart.text',
-    classes: 'shepherd-element--first',
-    highlightClass: 'intro',
-    btnBackText: 'stepButtons.skipWizard',
-    btnNextText: 'stepButtons.startWizard',
-    btnCollapseClass: 'd-none',
-    btnCloseClass: 'd-none',
-  },
-  {
-    id: 'step-env',
-    text: 'stepEnv.text',
-    classes: 'shepherd-element--align-right',
-    highlightClass: 'api-settings',
-    attachTo: {
-      element: '.js-onboardingWizard-environment',
-      on: 'top-start'
-    },
-    btnNextClass: 'shepherd-button--arrow-down',
-  },
-  {
-    id: 'step-mollie-connect',
-    title: 'stepMollieConnect.title',
-    text: 'stepMollieConnect.text',
-    highlightClass: 'api-settings',
-    btnBackText: 'stepButtons.loginMollieAccount',
-    btnNextText: 'stepButtons.createMollieAccount',
-    btnCollapseClass: 'd-none',
-    urlMollie: 'https://www.mollie.com/dashboard',
-  },
-  {
-    id: 'step-api-key',
-    text: 'stepApiKey.text',
-    classes: 'shepherd-element--align-right',
-    highlightClass: 'api-settings',
-    btnNextClass: 'shepherd-button--arrow-down',
-    attachTo: {
-      element: '.js-onboardingWizard-profile-api',
-      on: 'top-start'
-    },
-  },
-  {
-    id: 'step-checkout-config',
-    text: 'stepCheckoutConfig.text',
-    classes: 'step-6 shepherd-element--align-right',
-    highlightClass: 'store-settings',
-    btnNextText: 'stepButtons.next',
-  },
-  {
-    id: 'step-mollie-components',
-    text: 'stepMollieComponents.text',
-    classes: 'shepherd-element--align-right',
-    highlightClass: 'store-settings',
-    btnNextClass: 'shepherd-button--arrow-down',
-    attachTo: {
-      element: '.js-onboardingWizard-mollieComponents + label',
-      on: 'top-start'
-    },
-  },
-  {
-    id: 'step-mollie-payments',
-    text: 'stepMolliePayments.text',
-    classes: 'shepherd-element--align-right',
-    highlightClass: 'store-settings',
-    btnNextClass: 'shepherd-button--arrow-down',
-    attachTo: {
-      element: '.js-onboardingWizard-singleClick + label',
-      on: 'top-start'
-    },
-  },
-  {
-    id: 'step-payments-api',
-    text: 'stepPaymentsApi.text',
-    classes: 'shepherd-element--align-right',
-    highlightClass: 'payment-settings',
-    btnNextText: 'stepButtons.next',
-  },
-  {
-    id: 'step-payment-title',
-    text: 'stepPaymentTitle.text',
-    classes: 'step-9 shepherd-element--align-right',
-    highlightClass: 'payment-settings',
-    btnNextClass: 'shepherd-button--arrow-down',
-    attachTo: {
-      element: '.js-onboardingWizard-paymentName',
-      on: 'top-start'
-    },
-  },
-  {
-    id: 'step-image-upload',
-    text: 'stepImageUpload.text',
-    classes: 'step-14 shepherd-element--align-right',
-    highlightClass: 'payment-settings',
-    btnNextClass: 'shepherd-button--arrow-down',
-    attachTo: {
-      element: '.js-onboardingWizard-customizeMethodImage input',
-      on: 'top-start'
-    },
-  },
-  {
-    id: 'step-restrict-payment',
-    text: 'stepRestrictPayment.text',
-    classes: 'step-12 shepherd-element--align-right',
-    highlightClass: 'payment-settings',
-    btnNextClass: 'shepherd-button--arrow-down',
-    attachTo: {
-      element: '.js-onboardingWizard-countryRestriction',
-      on: 'top-start'
-    },
-  },
-  {
-    id: 'step-fees',
-    text: 'stepFees.text',
-    classes: 'step-13 shepherd-element--align-right',
-    highlightClass: 'payment-settings',
-    btnNextClass: 'shepherd-button--arrow-down',
-    attachTo: {
-      element: '.js-onboardingWizard-paymentFee .dropdown',
-      on: 'top-start'
-    },
-  },
-  {
-    id: 'step-finish-wizard',
-    title: 'stepFinishWizard.title',
-    text: 'stepFinishWizard.text',
-    highlightClass: 'payment-settings',
-    btnBackClass: 'd-none',
-    btnNextClass: 'mr-auto',
-    btnNextText: 'stepButtons.finishWizard',
-    btnCollapseClass: 'd-none',
-    btnCloseClass: 'd-none',
-  },
+	{
+		id: 'step-start',
+		title: 'stepStart.title',
+		text: 'stepStart.text',
+		classes: 'shepherd-element--first',
+		highlightClass: 'intro',
+		btnBackText: 'stepButtons.skipWizard',
+		btnNextText: 'stepButtons.startWizard',
+		btnCollapseClass: 'd-none',
+		btnCloseClass: 'd-none',
+	},
+	{
+		id: 'step-mollie-connect',
+		title: 'stepMollieConnect.title',
+		text: 'stepMollieConnect.text',
+		highlightClass: 'intro',
+		btnBackText: 'stepButtons.loginMollieAccount',
+		btnNextText: 'stepButtons.createMollieAccount',
+		btnCollapseClass: 'd-none',
+		urlMollie: 'https://www.mollie.com/dashboard',
+	},
+	{
+		id: 'step-env',
+		text: 'stepEnv.text',
+		classes: 'shepherd-element--align-right',
+		highlightClass: 'api-settings',
+		attachTo: {
+			element: '.js-onboardingWizard-environment',
+			on: 'top-start',
+		},
+		btnNextClass: 'shepherd-button--arrow-down',
+	},
+	{
+		id: 'step-api-key',
+		text: 'stepApiKey.text',
+		classes: 'shepherd-element--align-right',
+		highlightClass: 'api-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-profile-api',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'step-checkout-config',
+		text: 'stepCheckoutConfig.text',
+		classes: 'step-6 shepherd-element--align-right',
+		highlightClass: 'store-settings',
+		btnNextText: 'stepButtons.next',
+	},
+	{
+		id: 'step-mollie-components',
+		text: 'stepMollieComponents.text',
+		classes: 'shepherd-element--align-right',
+		highlightClass: 'store-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-mollieComponents',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'step-mollie-payments',
+		text: 'stepMolliePayments.text',
+		classes: 'shepherd-element--align-right',
+		highlightClass: 'store-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-singleClick',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'step-payments-api',
+		text: 'stepMethodConfig.text',
+		classes: 'shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextText: 'stepButtons.next',
+		attachTo: {
+			element: '.js-onboardingWizard-load-methods',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'step-payment-title',
+		text: 'stepPaymentTitle.text',
+		classes: 'step-9 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-paymentName',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'step-image-upload',
+		text: 'stepImageUpload.text',
+		classes: 'step-14 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-customizeMethodImage input',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'step-country-restriction',
+		text: 'stepCountryRestriction.text',
+		classes: 'step-12 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-countryRestriction',
+			on: 'top-start',
+		},
+	},
+	// payment type select
+	{
+		id: 'step-payment-method',
+		text: 'stepPaymentMethod.text',
+		classes: 'step-12 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-PaymentMethod',
+			on: 'top-start',
+		},
+	},
+	//
+	{
+		showOn: paymentTypeIndicator(
+			'#sylius_payment_method_gatewayConfig_mollieGatewayConfig_0_paymentType',
+			'ORDER_API'
+		),
+		id: 'step-order-number',
+		text: 'stepOrderNumber.text',
+		classes: 'step-12 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+	},
+	{
+		showOn: paymentTypeIndicator(
+			'#sylius_payment_method_gatewayConfig_mollieGatewayConfig_0_paymentType',
+			'ORDER_API'
+		),
+		id: 'step-order-api',
+		text: 'stepOrderAPI.text',
+		classes: 'step-12 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+	},
+	{
+		showOn: paymentTypeIndicator(
+			'#sylius_payment_method_gatewayConfig_mollieGatewayConfig_0_paymentType',
+			'PAYMENT_API'
+		),
+		id: 'step-payments-api',
+		text: 'stepPaymentsAPI.text',
+		classes: 'step-12 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+	},
+	//
+	{
+		id: 'step-fees',
+		text: 'stepFees.text',
+		classes: 'step-13 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '.js-onboardingWizard-paymentFee .dropdown',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'save',
+		text: 'stepSave.text',
+		classes: 'step-13 shepherd-element--align-right',
+		highlightClass: 'payment-settings',
+		btnNextClass: 'shepherd-button--arrow-down',
+		attachTo: {
+			element: '#sylius_save_changes_button',
+			on: 'top-start',
+		},
+	},
+	{
+		id: 'step-finish-wizard',
+		title: 'stepFinishWizard.title',
+		text: 'stepFinishWizard.text',
+		highlightClass: 'payment-settings',
+		btnBackClass: 'd-none',
+		btnNextClass: 'mr-auto',
+		btnNextText: 'stepButtons.finishWizard',
+		btnCollapseClass: 'd-none',
+		btnCloseClass: 'd-none',
+	},
 ];
--- src/Resources/assets/js/config/wizardTranslations.js
+++ src/Resources/assets/js/config/wizardTranslations.js
@@ -1,73 +1,92 @@
 export default {
-  stepStart: {
-    title: 'Let me help you',
-    text: 'Thank you for installing Mollie for payment services. This guide will take you through the configuration setup. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit nibh quis urna congue, et interdum nulla rutrum. Cras at justo ornare.'
-  },
-  stepEnv: {
-    text: 'TEST will be the default in the plugin. You only need to do the configuration once to have TEST + LIVE environments available. Try easily togging between the two.'
-  },
-  stepMollieConnect: {
-    title: 'Connect to your Mollie account',
-    text: 'To sync the Mollie plugin to your webshop you\'ll need Mollie API keys and Profile ID.'
-  },
-  stepApiKey: {
-    text: 'Fill in your correct details and click "TEST API Key" this will return a successful or failed result for both the LIVE and TEST environments. Learn about the difference between: Orders API or the Payments API'
-  },
-  stepCheckoutConfig: {
-    text: 'Webshop checkout Configurations, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper velit et urna gravida efficitur.'
-  },
-  stepMollieComponents: {
-    text: 'Enabling components, allows you to add the fields needed for credit card holder data to your own checkout. If you select NO, users will be redirected to the Mollie checkout page'
-  },
-  stepMolliePayments: {
-    text: 'Enabling single click payments remembers your consumer\'s payment preferences in order to expedite follow-up payments. Your consumer does not have to perform any additional actions to enjoy quick and easy payments.'
-  },
-  stepPaymentsApi: {
-    text: 'We\'ll go through setup with the Payments API first and then highlight differences if you choose to use the Orders API'
-  },
-  stepPaymentTitle: {
-    text: 'You can enter a custom title here - it will be displayed on your checkout page'
-  },
-  stepImageUpload: {
-    text: 'Upload a custom image for the payment method icon, this will be shown in the checkout page.'
-  },
-  stepRestrictPayment: {
-    text: 'Restrict/allow payment per individual countries.'
-  },
-  stepFees: {
-    text: 'In case you have fees that you are passing on to the consumer, you can add them here'
-  },
-  stepFinishWizard: {
-    title: '<i class="icon check circle"></i> You\'re all set!',
-    text: 'You\'re all done, you can now attempt a consumer order or your website.'
-  },
-  stepQuitConfirmation: {
-    title: 'Are you sure you want to quit ?',
-    text: 'You\'re all done, you can now attempt a consumer order or your website'
-  },
-  stepPaymentType: {
-    text: 'When using Payments API you may want additional details to help you match payments with customer orders -- you can enter those values here but make sure to use the correct tags provide in the text below'
-  },
-  stepPaymentDescription: {
-    text: 'Choose Payments API Learn about the difference between Orders API or the Payments API'
-  },
-  stepOrderApi: {
-    text: 'Select Orders API - this is Mollie\n' +
-    'suggested API to use for webshops b/c it allows you to create “orders”. An order contains the personal information of a customer (e.g. address) and products that the customer ordered. When an order is made, a corresponding payment will be created automatically.',
-  },
-  stepButtons: {
-    goBack: 'Go back',
-    skipWizard: 'Skip this, I know how it works',
-    startWizard: 'Start onboarding assistant <i class="icon angle right"></i>',
-    loginMollieAccount: 'Login to my account',
-    nextWithArrow: 'Next <i class="icon angle right"></i>',
-    next: 'Next',
-    createMollieAccount: 'Create a Mollie account <i class="icon angle right"></i>',
-    finishWizard: 'Start using Mollie plugin <i class="icon angle right"></i>',
-    quitConfirm: 'Quit the onboarding assistant',
-    quitCancel: 'Continue onboarding <i class="icon angle right"></i>',
-  },
-  common: {
-    open: 'Open'
-  }
+	stepStart: {
+		title: 'Let me help you',
+		text: 'Thank you for installing Mollie for payment services. This guide will take your through the configuration set-up.',
+	},
+	stepMollieConnect: {
+		title: 'Connect to your Mollie account',
+		text: "To sync the Mollie plugin to your webshop you'll need Mollie API keys and Profile ID.",
+	},
+	stepEnv: {
+		text: "Now that you're connected to Mollie we will configure the environment and credentials. Test will be the default environment in the plugin. You only need to do the configuration once to have TEST + LIVE environments available. Try easily togging between the two.",
+	},
+	stepApiKey: {
+		text: 'Fill in your correct details and click "TEST API Key" this will return a successful or failed result for both the LIVE and TEST environments',
+	},
+	stepCheckoutConfig: {
+		text: "Next, we'll set-up key settings for the checkout screen display on your webshop.",
+	},
+	stepMollieComponents: {
+		text: `Enabling components, allows you to add the fields needed for credit card holder data to your own checkout. If you select NO, users will be redirected to the Mollie checkout page. Learn more about Mollie components <a href="https://www.mollie.com/en/news/post/better-checkout-flows-with-mollie-components">here</a>.`,
+	},
+	stepMolliePayments: {
+		text: `Enabling single click payments remembers your consumer's payment preferences in order to expedite follow-up payments. Your consumer does not have to perform any additional actions to enjoy quick and easy payments. Learn more about single click payments <a href="https://help.mollie.com/hc/en-us/articles/115000671249-What-are-single-click-payments-and-how-does-it-work-"here</a>.`,
+	},
+	stepMethodConfig: {
+		text: 'Now time to customize features for individual payment methods. First, select the load methods button. Only the methods that are enabled in your Mollie account will display here. Then you use the enable/disable selector to control which will show on your webshop checkout. ',
+	},
+	stepPaymentTitle: {
+		text: 'For each method, you can enter a custom title here - it will be displayed on your webshop checkout page.',
+	},
+	stepImageUpload: {
+		text: 'Try uploading a custom image for the payment method icon. This will be shown in the wenshop checkout page.',
+	},
+	stepCountryRestriction: {
+		text: 'Here you can create filters for country specific payment methods - for example, if you want iDEAL only to show for Netherlands customers you choose "Select Countries" and select Netherlands.',
+	},
+	stepPaymentMethod: {
+		text: 'Per method, you can select which Mollie API to use to create payments. Click <a href="https://docs.mollie.com/orders/why-use-orders">here</a> to read about the differences between Orders and Payments API.',
+	},
+	stepOrderNumber: {
+		text: 'When using Payments API you may want additional details to help you match payments with customer orders -- you can enter those values here but make sure to use the correct tags provided in the text below',
+	},
+  stepOrdersAPI: {
+		text: 'Orders API <br>This is Mollie suggested API to use for webshops because it allows you to create “orders”. An order contains the personal information of a customer (e.g. address) and products that the customer ordered. When an order is made, a corresponding payment will be created automatically.',
+	},
+  stepPaymentsAPI: {
+		text: 'Payments API <br>Note: Payments API can not be used for methods such as Klarna',
+	},
+	stepFees: {
+		text: 'In case you have fees that you are passing on to the consumer, you can add them here',
+	},
+	stepSave: {
+		text: 'Remeber to save your configurations. ',
+	},
+	stepFinishWizard: {
+		title: '<i class="icon check circle"></i> You\'re all set!',
+		text: "You're all done, you can now attempt a consumer order or your website.",
+	},
+	stepQuitConfirmation: {
+		title: 'Are you sure you want to quit ?',
+		text: "You're all done, you can now attempt a consumer order or your website",
+	},
+	stepPaymentType: {
+		text: 'When using Payments API you may want additional details to help you match payments with customer orders -- you can enter those values here but make sure to use the correct tags provide in the text below',
+	},
+	stepPaymentDescription: {
+		text: 'Choose Payments API Learn about the difference between Orders API or the Payments API',
+	},
+	stepOrderApi: {
+		text:
+			'Select Orders API - this is Mollie\n' +
+			'suggested API to use for webshops b/c it allows you to create “orders”. An order contains the personal information of a customer (e.g. address) and products that the customer ordered. When an order is made, a corresponding payment will be created automatically.',
+	},
+	stepButtons: {
+		goBack: 'Go back',
+		skipWizard: 'Skip this, I know how it works',
+		startWizard:
+			'Start onboarding assistant <i class="icon angle right"></i>',
+		loginMollieAccount: 'Login to my account',
+		nextWithArrow: 'Next <i class="icon angle right"></i>',
+		next: 'Next',
+		createMollieAccount:
+			'Create a Mollie account <i class="icon angle right"></i>',
+		finishWizard:
+			'Start using Mollie plugin <i class="icon angle right"></i>',
+		quitConfirm: 'Quit the onboarding assistant',
+		quitCancel: 'Continue onboarding <i class="icon angle right"></i>',
+	},
+	common: {
+		open: 'Open',
+	},
 };

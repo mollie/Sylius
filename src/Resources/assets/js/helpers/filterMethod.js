dfd42551b4fbf53240f19cfeee55363e3ce6918e
admin <admin@admins-MacBook-Pro.local>
2021-05-27 10:28:40 +0200

    MOL-115 indent fix

--- src/Resources/assets/js/helpers/filterMethod.js
+++ src/Resources/assets/js/helpers/filterMethod.js
@@ -2,11 +2,11 @@ export const paymentTypeIndicator = (item, expectedValue) => {
 	const indicatedItem = document.querySelector(item);
 	const indicatedItemValue = indicatedItem.value;
 
-  return indicatedItemValue === expectedValue;
+    return indicatedItemValue === expectedValue;
 };
 
 export const methodLoadIndicator = (item) => {
 	const indicatedItem = document.querySelector(item);
   
-  return !indicatedItem;
+    return !indicatedItem;
 };

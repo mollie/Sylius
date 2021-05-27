77b6e6f984df98d91b092358f60f177e00892d62
admin <admin@admins-MacBook-Pro.local>
2021-05-27 08:44:20 +0200

    MOL-115 aditional cleanup

---
+++ src/Resources/assets/js/helpers/filterMethod.js
@@ -0,0 +1,19 @@
+export const paymentTypeIndicator = (item, expectedValue) => {
+	const indicatedItem = document.querySelector(`${item}`);
+	const indicatedItemValue = indicatedItem.value;
+	if (indicatedItemValue == `${expectedValue}`) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+export const methodLoadIndicator = (item) => {
+	const indicatedItem = document.querySelector(`${item}`);
+	if (indicatedItem ) {
+		return false;
+	} else {
+		return true;
+	}
+};
+

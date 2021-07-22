1d58b013c2bb6f2be4b0dba31632f7cf5dab4627
Harvel218 <gacekrzysztof.218@gmail.com>
2021-07-22 13:31:45 +0200

    rebase from master

--- src/Resources/assets/admin/js/index.js
+++ src/Resources/assets/admin/js/index.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD:src/Resources/assets/js/main.js
-=======
 import './molliePayments/main';
 import './onboardingWizard/main';
->>>>>>> MOL-134 webpack config, project setup changes etc.:src/Resources/assets/admin/js/main.js

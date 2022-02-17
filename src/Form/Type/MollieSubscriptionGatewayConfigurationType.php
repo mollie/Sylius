cd42f3a27a5e9ae84d62fe8d8318b1daff25ff58
Marcin Bonk <marvcin@gmail.com>
2022-02-17 15:15:30 +0100

    Add missing class

---
+++ src/Form/Type/MollieSubscriptionGatewayConfigurationType.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusMolliePlugin\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\Regex;
+
+final class MollieSubscriptionGatewayConfigurationType extends AbstractType
+{
+    public function getParent(): string
+    {
+        return MollieGatewayConfigurationType::class;
+    }
+}

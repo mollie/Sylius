48ca31772991a569ce653c595208912bf92060f7
Marcin Kukliński <marcin.kuklinski@bitbag.pl>
2022-04-14 01:48:25 +0200

    Writing functional test for order refunds webhook [WIP]

---
+++ tests/PHPUnit/Functional/FunctionalTestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\BitBag\SyliusMolliePlugin\PHPUnit\Functional;
+
+use ApiTestCase\JsonApiTestCase;
+
+class FunctionalTestCase extends JsonApiTestCase
+{
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->dataFixturesPath = __DIR__ . '/DataFixtures';
+    }
+}

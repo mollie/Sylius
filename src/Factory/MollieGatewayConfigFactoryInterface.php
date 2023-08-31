861358bb18afdcc9e69c0fd0387d83d12c6392b1
Mark Beazley <mark@bluebanana.com>
2023-08-31 13:31:00 +0100

    Fix some issues flagged by Symfony container linter.

--- src/Factory/MollieGatewayConfigFactoryInterface.php
+++ src/Factory/MollieGatewayConfigFactoryInterface.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 
 namespace SyliusMolliePlugin\Factory;
 
+use Sylius\Component\Resource\Factory\FactoryInterface;
 use SyliusMolliePlugin\Entity\GatewayConfigInterface;
 use SyliusMolliePlugin\Entity\MollieGatewayConfigInterface;
 use SyliusMolliePlugin\Payments\Methods\MethodInterface;
 
-interface MollieGatewayConfigFactoryInterface
+interface MollieGatewayConfigFactoryInterface extends FactoryInterface
 {
     public function create(
         MethodInterface $method,

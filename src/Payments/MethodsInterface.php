8eba70fd9d7efcb214009f0b013dc789f105a71c
Mark Beazley <mark@bluebanana.com>
2023-10-26 16:13:51 +0100

    Add Klarna one support.

--- src/Payments/MethodsInterface.php
+++ src/Payments/MethodsInterface.php
@@ -18,6 +18,7 @@ use SyliusMolliePlugin\Payments\Methods\Giropay;
 use SyliusMolliePlugin\Payments\Methods\Ideal;
 use SyliusMolliePlugin\Payments\Methods\In3;
 use SyliusMolliePlugin\Payments\Methods\Kbc;
+use SyliusMolliePlugin\Payments\Methods\KlarnaOne;
 use SyliusMolliePlugin\Payments\Methods\Klarnapaylater;
 use SyliusMolliePlugin\Payments\Methods\KlarnaPayNow;
 use SyliusMolliePlugin\Payments\Methods\Klarnasliceit;
@@ -41,6 +42,7 @@ interface MethodsInterface
         Giropay::class,
         Ideal::class,
         Kbc::class,
+        KlarnaOne::class,
         Klarnapaylater::class,
         Klarnasliceit::class,
         KlarnaPayNow::class,
